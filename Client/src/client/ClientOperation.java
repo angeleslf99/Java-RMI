62961f8e4d312bc7808b3d3124f8ab93ce4dd59e
Ma. de los Angeles Lira Flores <angelitaflor2018@gmail.com>
2021-05-23 22:52:59 -0500

    Commit
    Se agrega el codigo faltante a nuestro proyecto

--- Client/src/client/ClientOperation.java
+++ Client/src/client/ClientOperation.java
@@ -1,27 +1,43 @@
-package client;
+package rmiclient;
 
-import model.Contact;
-import rmiinterface.RMIInterface;
-
-import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.JOptionPane;
+
+import com.itextpdf.text.DocumentException;
+import rmiinterface.RMIInterface;
+import rmiclient.GeneratePDF;
 
 public class ClientOperation {
-
     private static RMIInterface look_up;
 
     public static void main(String[] args)
-            throws MalformedURLException, RemoteException, NotBoundException {
+            throws IOException, NotBoundException, DocumentException {
 
         look_up = (RMIInterface) Naming.lookup("//localhost/MyServer");
-        //String txt = JOptionPane.showInputDialog("What is your name?");
+        String ruta = JOptionPane.showInputDialog("Que ruta mostrar");
 
-        Contact response = look_up.getContact();
-        JOptionPane.showMessageDialog(null, response.getName() + " " + response.getTelefone());
+        // Crear objeto de GeneratePDF
+        GeneratePDF pdf = new GeneratePDF();
+
+        //String response = look_up.helloTo(txt);
+        ArrayList<String> response = look_up.obtenerArchivos(ruta);
+        pdf.crearPDF(new File("reporte.pdf"), response, ruta);
+        Iterator<String> Iterator = response.iterator();
+        while(Iterator.hasNext()){
+            String elemento = Iterator.next();
+            System.out.println(elemento);
+        }
+        //System.out.println(response);
+        //JOptionPane.showMessageDialog(null, response);
 
     }
-
-}
+}}
